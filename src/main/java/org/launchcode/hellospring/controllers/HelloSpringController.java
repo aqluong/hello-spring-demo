2af620946f675a5458184f782de795c3d13f9ff3
Anthony Luong <anthony@Anthonys-MacBook-Pro.local>
2023-02-05 19:42:44 -0500

    Working on Templets 13.2

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -0,0 +1,31 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+public class HelloSpringController {
+
+        // Responds to /hello?name=LaunchCode
+        @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+        @ResponseBody
+        public String hello(@RequestParam String name) {
+            return "Hello, " + name + "!";
+        }
+
+        // Responds to /hello/LaunchCode
+        @GetMapping("hello/{name}")
+        @ResponseBody
+        public String helloAgain(@PathVariable String name) {
+            return "Hello, " + name + "!";
+        }
+        @GetMapping("form")
+        public String helloForm() {
+            return "form";
+
+    }
+
+}
